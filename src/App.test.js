e907f0a83eb0f7926a0f45a64eb87e1199e149b6
beach1208 <nk.bg.1208@gmail.com>
2021-08-06 16:20:38 +0900

    Added config

--- src/App.test.js
+++ src/App.test.js
@@ -89,15 +89,3 @@ test('Clicked disabled button has grey background and reverts to blue', () => {
    expect(colorButton).toHaveStyle('background-color: blue');
 
 });
-
-describe('spaces before camel-case capital letters', () => {
-  test('Works for no inner capital letters', () => {
-    expect(replaceCamelWithSpaces('Red')).toBe('Red');
-  });
-  test('Works for one inner capital letter', () => {
-    expect(replaceCamelWithSpaces('MidnightBlue')).toBe('Midnight Blue');
-  });
-  test('Works for multiple inner capital letters', () => {
-    expect(replaceCamelWithSpaces('MediumVioletRed')).toBe('Medium Violet Red');
-  });
-})
